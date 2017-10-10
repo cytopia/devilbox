1fa6bc700842c3c926b78d1b9c1da63d7ab59570
cytopia <cytopia@everythingcli.org>
2017-10-10 08:56:10 +0200

    REL-0.12 Bump version for upcoming release

--- .devilbox/www/config.php
+++ .devilbox/www/config.php
@@ -14,7 +14,7 @@ putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1');
 
 
 $DEVILBOX_VERSION = 'v0.12';
-$DEVILBOX_DATE = '2017-10-08';
+$DEVILBOX_DATE = '2017-10-10';
 $DEVILBOX_API_PAGE = 'devilbox-api/status.json';
 
 //
