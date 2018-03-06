5a23138268c79f0f5d96f0d3dd134d939d15c972
cytopia <cytopia@everythingcli.org>
2018-03-06 08:50:43 +0100

    Update update-docker.sh

--- update-docker.sh
+++ update-docker.sh
@@ -22,12 +22,12 @@ docker pull cytopia/bind:${TAG}
 ### PHP
 ###
 TAG="$( grep '^[[:space:]]*image:.*\${PHP_SERVER' "${CWD}/docker-compose.yml" | sed 's/^.*://g' )"
-docker pull cytopia/php-fpm-5.4:${TAG}
-docker pull cytopia/php-fpm-5.5:${TAG}
-docker pull cytopia/php-fpm-5.6:${TAG}
-docker pull cytopia/php-fpm-7.0:${TAG}
-docker pull cytopia/php-fpm-7.1:${TAG}
-docker pull cytopia/hhvm-latest:${TAG}
+docker pull devilbox/php-fpm:5.4-work
+docker pull devilbox/php-fpm:5.5-work
+docker pull devilbox/php-fpm:5.6-work
+docker pull devilbox/php-fpm:7.0-work
+docker pull devilbox/php-fpm:7.1-work
+#docker pull cytopia/hhvm-latest:${TAG}
 
 ###
 ### HTTPD
