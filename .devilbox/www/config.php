eff72f41a9b96a2f0d978ed7cc751a3358e09be5
cytopia <cytopia@everythingcli.org>
2018-12-15 17:12:56 +0100

    Update version

--- .devilbox/www/config.php
+++ .devilbox/www/config.php
@@ -14,7 +14,7 @@ putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1');
 
 
 $DEVILBOX_VERSION = 'v0.15';
-$DEVILBOX_DATE = '2018-11-03';
+$DEVILBOX_DATE = '2018-12-15';
 $DEVILBOX_API_PAGE = 'devilbox-api/status.json';
 
 //
