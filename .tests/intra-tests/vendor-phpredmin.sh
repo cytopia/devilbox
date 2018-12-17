e925c47b78e8ae6c057d9c1aeaecd8e3ec533a6a
cytopia <cytopia@everythingcli.org>
2018-12-17 11:01:29 +0100

    Ensure to test subdirectories of PHPRedmin

--- .tests/intra-tests/vendor-phpredmin.sh
+++ .tests/intra-tests/vendor-phpredmin.sh
@@ -39,3 +39,34 @@ if [ "$(curl -sS localhost${URL} | tac | tac | grep -Ec "Strings|Hashes|Lists|Se
 else
 	printf "\r[OK]   Fetch ${URL} (1 round)\n"
 fi
+
+
+###
+### Ensure PHPRedmin works in subdirectory
+###
+
+URL="/vendor/phpredmin/public/index.php/welcome/info/0/0"
+
+printf "[TEST] Fetch ${URL}"
+# 1st Try
+if [ "$(curl -sS localhost${URL} | tac | tac | grep -Ec "(Version:)|(Mode:)|(Role:)|(OS:)|(Uptime:)")" != "5" ]; then
+	# 2nd Try
+	sleep 1
+	if [ "$(curl -sS localhost${URL} | tac | tac | grep -Ec "(Version:)|(Mode:)|(Role:)|(OS:)|(Uptime:)")" != "5" ]; then
+		# 3rd Try
+		sleep 1
+		if [ "$(curl -sS localhost${URL} | tac | tac | grep -Ec "(Version:)|(Mode:)|(Role:)|(OS:)|(Uptime:)")" != "5" ]; then
+			printf "\r[FAIL] Fetch ${URL}\n"
+			curl -sS localhost/${URL} | tac | tac | grep -Ec "(Version:)|(Mode:)|(Role:)|(OS:)|(Uptime:)" || true
+			curl -sS localhost/${URL} || true
+			curl -sSI localhost/${URL} || true
+			exit 1
+		else
+			printf "\r[OK]   Fetch ${URL} (3 rounds)\n"
+		fi
+	else
+		printf "\r[OK]   Fetch ${URL} (2 rounds)\n"
+	fi
+else
+	printf "\r[OK]   Fetch ${URL} (1 round)\n"
+fi
