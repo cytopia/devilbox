67c3d8bd131e113318073a387c74ad5e9a8bfbf6
Szántó Zoltán <mrbig00@gmail.com>
2019-01-18 11:27:58 +0200

    Config phpmyadmin 4.8.4 to autologin

--- .devilbox/www/htdocs/vendor/phpmyadmin-4.8.4/config.inc.php
+++ .devilbox/www/htdocs/vendor/phpmyadmin-4.8.4/config.inc.php
@@ -31,7 +31,9 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = getenv('MYSQL_ROOT_PASSWORD'); 
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['compress'] = false;
