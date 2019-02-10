e78dca5684e128719b4d84dcddc12399cd771980
cytopia <cytopia@everythingcli.org>
2019-02-10 14:29:15 +0100

    Bump version

--- .devilbox/www/config.php
+++ .devilbox/www/config.php
@@ -13,8 +13,8 @@ error_reporting(-1);
 putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1');
 
 
-$DEVILBOX_VERSION = 'v0.15';
-$DEVILBOX_DATE = '2019-01-30';
+$DEVILBOX_VERSION = 'v1.0.0-alpha1';
+$DEVILBOX_DATE = '2019-02-10';
 $DEVILBOX_API_PAGE = 'devilbox-api/status.json';
 
 //
