326980fb2443113c9c7bfdae84d25b618e04b909
Parham Ghaffarian <parham@gouconenct.com>
2020-10-18 09:30:43 -0400

    Added MySQL, PHP, and Apache config files with a sample wp-config php for dev

---
+++ data/www/uconnectlabs/wp-config-env-dev-sample.php
@@ -0,0 +1,46 @@
+<?php
+define('WP_HOME','http://www.uconnectlabs.test');
+define('WP_SITEURL', WP_HOME);
+
+define('DOMAIN_CURRENT_SITE', 'www.uconnectlabs.test');
+define('BLOG_ID_CURRENT_SITE', 2);
+
+define('DB_NAME', 'uconnectlabs');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', '127.0.0.1');
+
+#define('DISABLE_ADMIN_REWRITE', true);
+
+define('NOINDEX', true);
+define('DONT_VERIFY_SCHOOL_EMAIL', true);
+
+define('WP_CACHE', false);
+
+define('UC_GA_PROPERTY_ID', 'UA-19563948-7');
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', true);
+
+define('UC_SMTP_HOST', '127.0.0.1'); // VVV MailHog
+define('UC_SMTP_PORT', 1025); // VVV MailHog
+define('UC_SMTP_AUTH', false); //
+
+//SendGrid test.uconnectlabs.com API key
+//define( 'UC_SMTP_PASS', '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' );
+
+define('DYNCDN_DOMAINS', ' cdn.uconnectlabs.test');
+define( 'WP_CRON_LOCK_TIMEOUT', 60 );
+
+define( 'UC_RECAPTCHA_VERIFY_HOST', false );
+
+// Define some variables that are used when developing - DEV_LOCAL_SERVER_DOMAIN should
+// be pointed to the office IP address, with ports 80 and 443 pointing to a dev laptop.
+define( 'DEV_LOCAL_SERVER_USER', 'parham' );
+define( 'DEV_LOCAL_SERVER_DOMAIN', DEV_LOCAL_SERVER_USER . '.dev.loc.uconnectlabs.com' );
+
+//define( 'ENABLE_TRACING_CODE', true);
+
+//define( 'UC_OBJECT_CACHE', 'redis' );
+
+define( 'QM_DISABLE_ERROR_HANDLER', true );
