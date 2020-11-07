2c8c8f2fa54d7f12d9a26970a3b9275c2f82182c
cytopia <cytopia@everythingcli.org>
2020-11-07 11:34:52 +0100

    Update Readme

--- .tests/scripts/get-modules.sh
+++ .tests/scripts/get-modules.sh
@@ -157,6 +157,17 @@ if ! PHP80_BASE="$( run "\
 	exit 1
 fi
 
+if ! PHP81_BASE="$( run "\
+	curl -sS 'https://raw.githubusercontent.com/devilbox/docker-php-fpm/${PHP_TAG}/README.md' \
+	| tac \
+	| tac \
+	| grep -E '81-base' \
+	| sed \
+		-e 's/.*\">//g' \
+		-e 's/<.*//g'" "${RETRIES}" )"; then
+	>&2 echo "Failed to retrieve modules for PHP 8.1"
+	exit 1
+fi
 
 ###
 ### Get PHP mods modules (5 rounds)
@@ -294,11 +305,22 @@ if ! PHP80_MODS="$( run "\
 	exit 1
 fi
 
+if ! PHP81_MODS="$( run "\
+	curl -sS 'https://raw.githubusercontent.com/devilbox/docker-php-fpm/${PHP_TAG}/README.md' \
+	| tac \
+	| tac \
+	| grep -E '81-mods' \
+	| sed \
+		-e 's/.*\">//g' \
+		-e 's/<.*//g'" "${RETRIES}" )"; then
+	>&2 echo "Failed to retrieve modules for PHP 8.1"
+	exit 1
+fi
 
 ###
 ### Todo: add ioncube
 ###
-MODS="$( echo "${PHP52_MODS}, ${PHP53_MODS}, ${PHP54_MODS}, ${PHP55_MODS}, ${PHP56_MODS}, ${PHP70_MODS}, ${PHP71_MODS}, ${PHP72_MODS}, ${PHP73_MODS}, ${PHP74_MODS}, ${PHP80_MODS}" | sed 's/,/\n/g' | sed -e 's/^\s*//g' -e 's/\s*$//g' | sort -u )"
+MODS="$( echo "${PHP52_MODS}, ${PHP53_MODS}, ${PHP54_MODS}, ${PHP55_MODS}, ${PHP56_MODS}, ${PHP70_MODS}, ${PHP71_MODS}, ${PHP72_MODS}, ${PHP73_MODS}, ${PHP74_MODS}, ${PHP80_MODS}, ${PHP81_MODS}" | sed 's/,/\n/g' | sed -e 's/^\s*//g' -e 's/\s*$//g' | sort -u )"
 
 
 ###
@@ -311,10 +333,11 @@ E="🗸"  # Enabled mods modules (can be disabled)
 D="d"  # Disabled modules (can be enabled)
 U=" "  # Unavailable
 
-echo "| Modules        | PHP 5.2 | PHP 5.3 | PHP 5.4 | PHP 5.5 | PHP 5.6 | PHP 7.0 | PHP 7.1 | PHP 7.2 | PHP 7.3 | PHP 7.4 | PHP 8.0 |"
-echo "|----------------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|"
+echo "| Modules                       | PHP 5.2 | PHP 5.3 | PHP 5.4 | PHP 5.5 | PHP 5.6 | PHP 7.0 | PHP 7.1 | PHP 7.2 | PHP 7.3 | PHP 7.4 | PHP 8.0 | PHP 8.1 |"
+echo "|-------------------------------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|"
 echo "${MODS}" | while read -r line; do
-	printf "| %-15s%s" "${line}" "|"
+    # Print current module
+	printf "| %-30s%s" "<small>${line}</small>" "|"
 
 	# ---------- PHP 5.2 ----------#
 	if echo ",${PHP52_MODS}," | sed 's/,\s/,/g' | grep -Eq ",${line},"; then
@@ -472,6 +495,21 @@ echo "${MODS}" | while read -r line; do
 			printf "    %s    |" "${D}"      # Currently disabled
 		else
 			if echo ",${PHP80_BASE}," | sed 's/,\s/,/g' | grep -Eq ",${line},"; then
+				printf "    %s    |" "${B}"  # Enabled, but cannot be disabled
+			else
+				printf "    %s    |" "${E}"  # Enabled, can be disabled
+			fi
+		fi
+	else
+		printf "    %s    |" "${U}"          # Not available
+	fi
+
+	# ---------- PHP 8.1 ----------#
+	if echo ",${PHP81_MODS}," | sed 's/,\s/,/g' | grep -Eq ",${line},"; then
+		if echo "${DISABLED}" | grep -Eq ",${line},"; then
+			printf "    %s    |" "${D}"      # Currently disabled
+		else
+			if echo ",${PHP81_BASE}," | sed 's/,\s/,/g' | grep -Eq ",${line},"; then
 				printf "    %s    |" "${B}"  # Enabled, but cannot be disabled
 			else
 				printf "    %s    |" "${E}"  # Enabled, can be disabled
