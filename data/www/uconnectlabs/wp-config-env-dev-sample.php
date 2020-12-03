a542a732a821285ee0a63af0a3bfa17d54283c05
Parham Ghaffarian <parham@gouconenct.com>
2020-12-03 18:16:32 -0500

    Added MailHog

--- data/www/uconnectlabs/wp-config-env-dev-sample.php
+++ data/www/uconnectlabs/wp-config-env-dev-sample.php
@@ -1,35 +1,36 @@
 <?php
-define('WP_HOME','http://www.uconnectlabs.test');
-define('WP_SITEURL', WP_HOME);
+define( 'WP_HOME', 'http://www.uconnectlabs.test' );
+define( 'WP_SITEURL', WP_HOME );
 
-define('DOMAIN_CURRENT_SITE', 'www.uconnectlabs.test');
-define('BLOG_ID_CURRENT_SITE', 2);
+define( 'DOMAIN_CURRENT_SITE', 'www.uconnectlabs.test' );
+define( 'BLOG_ID_CURRENT_SITE', 2 );
 
-define('DB_NAME', 'uconnectlabs');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', '127.0.0.1');
+define( 'DB_NAME', 'uconnectlabs' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '127.0.0.1' );
 
 #define('DISABLE_ADMIN_REWRITE', true);
 
-define('NOINDEX', true);
-define('DONT_VERIFY_SCHOOL_EMAIL', true);
+define( 'NOINDEX', true );
+define( 'DONT_VERIFY_SCHOOL_EMAIL', true );
 
-define('WP_CACHE', false);
+define( 'WP_CACHE', false );
 
-define('UC_GA_PROPERTY_ID', 'UA-19563948-7');
+define( 'UC_GA_PROPERTY_ID', 'UA-19563948-7' );
 
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true);
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
 
-define('UC_SMTP_HOST', '127.0.0.1'); // VVV MailHog
-define('UC_SMTP_PORT', 1025); // VVV MailHog
-define('UC_SMTP_AUTH', false); //
+//View emails at http://localhost:8025
+define( 'UC_SMTP_HOST', 'mailhog' ); // Devilbox MailHog
+define( 'UC_SMTP_PORT', 1025 ); // Devilbox MailHog
+define( 'UC_SMTP_AUTH', false ); //
 
-//SendGrid test.uconnectlabs.com API key
+// SendGrid test.uconnectlabs.com API key
 //define( 'UC_SMTP_PASS', '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' );
 
-define('DYNCDN_DOMAINS', ' cdn.uconnectlabs.test');
+define( 'DYNCDN_DOMAINS', ' cdn.uconnectlabs.test' );
 define( 'WP_CRON_LOCK_TIMEOUT', 60 );
 
 define( 'UC_RECAPTCHA_VERIFY_HOST', false );
