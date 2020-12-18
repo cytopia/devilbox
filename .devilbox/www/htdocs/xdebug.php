e65685b6046af12a76668c62ce1cf35f58682fa8
Nicola Galgano <optimus4joomla@gmail.com>
2020-12-18 17:16:28 +0100

    with header and footer

--- .devilbox/www/htdocs/xdebug.php
+++ .devilbox/www/htdocs/xdebug.php
@@ -1,2 +1,26 @@
-<?php
-xdebug_info();
+<?php require '../config.php'; ?>
+<?php loadClass('Helper')->authPage(); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php echo loadClass('Html')->getHead(); ?>
+	</head>
+
+	<body>
+		<?php echo loadClass('Html')->getNavbar(); ?>
+
+		<div class="container">
+
+			<div class="row">
+				<div class="col-md-12">
+					<style>
+						body {width: 100% !important;}
+					</style>
+					<?php xdebug_info(); ?>
+				</div>
+			</div>
+		</div><!-- /.container -->
+
+		<?php echo loadClass('Html')->getFooter(); ?>
+	</body>
+</html>
