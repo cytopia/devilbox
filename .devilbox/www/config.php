7bd6b34698e69a046dbcb15958eb0a0695386d3a
cytopia <cytopia@everythingcli.org>
2022-01-24 19:39:09 +0100

    Update project files

--- .devilbox/www/config.php
+++ .devilbox/www/config.php
@@ -13,8 +13,8 @@ error_reporting(-1);
 putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1');
 
 
-$DEVILBOX_VERSION = 'v1.9.2';
-$DEVILBOX_DATE = '2021-06-04';
+$DEVILBOX_VERSION = 'v1.9.3';
+$DEVILBOX_DATE = '2022-01-25';
 $DEVILBOX_API_PAGE = 'devilbox-api/status.json';
 
 //
