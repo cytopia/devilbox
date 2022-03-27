e73d3a74c2e01fe20dd010f97628e971cf7a8d5a
cytopia <cytopia@everythingcli.org>
2022-03-27 04:25:52 +0200

    Bump version

--- .devilbox/www/config.php
+++ .devilbox/www/config.php
@@ -13,8 +13,8 @@ error_reporting(-1);
 putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1');
 
 
-$DEVILBOX_VERSION = 'v1.11.0';
-$DEVILBOX_DATE = '2022-03-22';
+$DEVILBOX_VERSION = 'v1.12.0';
+$DEVILBOX_DATE = '2022-04-01';
 $DEVILBOX_API_PAGE = 'devilbox-api/status.json';
 
 //
