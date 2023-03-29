a47d5054d3e89b5ab71b33783743900430e35830
Vikas Kapadiya <vikas@kapadiya.net>
2023-03-30 00:37:52 +0530

    Update shell.bat

--- shell.bat
+++ shell.bat
@@ -1,2 +1,7 @@
+set php=php
 
-docker-compose exec --user devilbox php  /bin/sh -c "cd /shared/httpd; exec bash -l"
+if not "%~1"=="" (
+    set php=%1
+)
+
+docker-compose exec --user devilbox %php% /bin/sh -c "cd /shared/httpd; exec bash -l"
